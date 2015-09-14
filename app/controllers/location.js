8c508c552fad6a4cee1ad97589a3c85f3465202e
sumedhashyam <shyam@sumedhasoftech.com>
2015-09-14 11:57:17 +0530

    t

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -1,28 +1,34 @@
 
 app.controller('locationCtrl', function($scope,$rootScope,dataSVC,cordovaGeolocationService) {
 	$scope.isLoading=false;
-	if(cordovaGeolocationService.checkGeolocationAvailability()){
-		alert('a')
-		$scope.isLoading=true;
-		cordovaGeolocationService.getCurrentPosition(function(position){
-			 alert('Latitude: '          + position.coords.latitude          + '\n' +
-			  'Longitude: '         + position.coords.longitude         + '\n' +
-			  'Altitude: '          + position.coords.altitude          + '\n' +
-			  'Accuracy: '          + position.coords.accuracy          + '\n' +
-			  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-			  'Heading: '           + position.coords.heading           + '\n' +
-			  'Speed: '             + position.coords.speed             + '\n' +
-			  'Timestamp: '         + position.timestamp                + '\n');
-			
-		},function(error){
-			alert('code: '    + error.code    + '\n' +
-			  'message: ' + error.message + '\n');
-			
-		},null);
-	}
-	else
-	{
-		alert('na')
+	cordova.plugins.diagnostic.isLocationEnabled(function(enabled){
+			if(cordovaGeolocationService.checkGeolocationAvailability()){
+				alert('a')
+				$scope.isLoading=true;
+				cordovaGeolocationService.getCurrentPosition(function(position){
+					 alert('Latitude: '          + position.coords.latitude          + '\n' +
+					  'Longitude: '         + position.coords.longitude         + '\n' +
+					  'Altitude: '          + position.coords.altitude          + '\n' +
+					  'Accuracy: '          + position.coords.accuracy          + '\n' +
+					  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+					  'Heading: '           + position.coords.heading           + '\n' +
+					  'Speed: '             + position.coords.speed             + '\n' +
+					  'Timestamp: '         + position.timestamp                + '\n');
+					
+				},function(error){
+					alert('code: '    + error.code    + '\n' +
+					  'message: ' + error.message + '\n');
+					
+				},null);
+		}
+		else
+		{
+			alert('na')
+			cordova.plugins.diagnostic.switchToLocationSettings();
+		}
+	}, function(error){
+			alert('na2')
 		cordova.plugins.diagnostic.switchToLocationSettings();
-	}
+	});
+	
 });
