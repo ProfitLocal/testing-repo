aa198a9035af68314464511aee4a9083aadc2a8b
sumedhashyam <shyam@sumedhasoftech.com>
2015-09-14 15:59:52 +0530

    t

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -1,42 +1,43 @@
 
 app.controller('locationCtrl', function($scope,$rootScope,dataSVC,cordovaGeolocationService) {
-	$scope.isLoading=true;	
+	$scope.isLoading=false;	
 	
-	//cordova.plugins.diagnostic.isLocationEnabled(function(enabled){//only android
-	//	alert(enabled)
-		//$scope.isLoading=!enabled;
-			if(cordovaGeolocationService.checkGeolocationAvailability()){				
-				
-				var w=cordovaGeolocationService.getCurrentPosition(function(position){
-					 alert('Latitude: '          + position.coords.latitude          + '\n' +
-					  'Longitude: '         + position.coords.longitude         + '\n' +
-					  'Altitude: '          + position.coords.altitude          + '\n' +
-					  'Accuracy: '          + position.coords.accuracy          + '\n' +
-					  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-					  'Heading: '           + position.coords.heading           + '\n' +
-					  'Speed: '             + position.coords.speed             + '\n' +
-					  'Timestamp: '         + position.timestamp                + '\n');
-					  	cordovaGeolocationService.clearWatch(w);				
-				},function(error){
-					alert('code: '    + error.code    + '\n' +
-					  'message: ' + error.message + '\n');
-					
-			cordova.plugins.diagnostic.switchToLocationSettings();
-				},null);
-		}
-		else
-		{
-			cordova.plugins.diagnostic.switchToLocationSettings();
-		}
-		document.addEventListener("resume", function() {
-			alert('a');
-		});
-	//}, function(error){
-			//alert('na2')
-	//	cordova.plugins.diagnostic.switchToLocationSettings();
-	//});
 	$scope.openSetting=function(){
 		
 			cordova.plugins.diagnostic.switchToLocationSettings();
 	}
+	function checkConnection=function(){
+		
+		cordova.plugins.diagnostic.isLocationEnabled(function(enabled){//only android			
+			$scope.isLoading=!enabled;
+				if(enabled){	
+					
+					var w=cordovaGeolocationService.getCurrentPosition(function(position){
+						 alert('Latitude: '          + position.coords.latitude          + '\n' +
+						  'Longitude: '         + position.coords.longitude         + '\n' +
+						  'Altitude: '          + position.coords.altitude          + '\n' +
+						  'Accuracy: '          + position.coords.accuracy          + '\n' +
+						  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+						  'Heading: '           + position.coords.heading           + '\n' +
+						  'Speed: '             + position.coords.speed             + '\n' +
+						  'Timestamp: '         + position.timestamp                + '\n');
+										
+					},function(error){
+						alert('code: '    + error.code    + '\n' +
+						  'message: ' + error.message + '\n');
+						
+					},null);
+			}		
+			
+		}, function(error){
+				//alert('na2')
+			//cordova.plugins.diagnostic.switchToLocationSettings();
+		});
+	}
+	document.addEventListener("deviceready", function() {
+		checkConnection();
+		document.addEventListener("resume", function() {
+			checkConnection();
+		});
+	});
 });
