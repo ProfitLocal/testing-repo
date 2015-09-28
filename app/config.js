83ccf184fad0cb11daa0dbde0de9c1034a8a211f
sumedhashyam <shyam@sumedhasoftech.com>
2015-09-28 15:05:38 +0530

    t

--- app/config.js
+++ app/config.js
@@ -2,8 +2,8 @@ var app =
     angular.module('app')
         .config(
         [
-            '$controllerProvider', '$compileProvider', '$filterProvider', '$provide','$httpProvider',
-            function($controllerProvider, $compileProvider, $filterProvider, $provide,$httpProvider) {
+            '$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
+            function($controllerProvider, $compileProvider, $filterProvider, $provide) {
                 app.controller = $controllerProvider.register;
                 app.directive = $compileProvider.directive;
                 app.filter = $filterProvider.register;
@@ -11,9 +11,7 @@ var app =
                 app.service = $provide.service;
                 app.constant = $provide.constant;
                 app.value = $provide.value;
-				$httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.headers.common = 'Content-Type: application/json';
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+			
             }
         ]);
 
